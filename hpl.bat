281b18eb66869fad14d0d381274db084c2e73a75
kawikawika <kawikabarabin@gmail.com>
2022-02-15 14:39:13 -0500

    Update hpl.bat

--- hpl.bat
+++ hpl.bat
@@ -12,4 +12,4 @@ export OMP_NUM_THREADS=1
 export OMP_PLACES=cores
 export OMP_PROC_BIND=close
 
-srun -u -c $OMP_NUM_THREADS --distribution=block:block --cpu-bind=none ./place.sh ./xhpl | tee hpl.out
+python srun.py | tee hpl.out
